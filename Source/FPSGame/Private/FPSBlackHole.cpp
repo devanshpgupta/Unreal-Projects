a62e9add737a74117f8a8393c103c58c03e05a93
Devansh Gupta <devanshpgupta@outlook.com>
2018-09-24 19:59:32 -0400

    git clone https://github.com/devanshpgupta/Unreal-Projects.git

--- Source/FPSGame/Private/FPSBlackHole.cpp
+++ Source/FPSGame/Private/FPSBlackHole.cpp
@@ -9,6 +9,7 @@ AFPSBlackHole::AFPSBlackHole()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+
 }
 
 // Called when the game starts or when spawned
